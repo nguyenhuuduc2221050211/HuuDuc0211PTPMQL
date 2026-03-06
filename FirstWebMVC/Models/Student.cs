653f629d35ef676d4a10494bbc5ace7ae3c38cc5
Huuduc <nduc53557@gmail.com>
2026-03-06 20:49:04 +0700

    Bai Thuc Hanh 5

--- FirstWebMVC/Models/Student.cs
+++ FirstWebMVC/Models/Student.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FirstWebMVC.Models
 {
     public class Student
     {
+        [Key]
         public string Studentcode {get; set;}
         public string Fullname {get; set;}
     }
